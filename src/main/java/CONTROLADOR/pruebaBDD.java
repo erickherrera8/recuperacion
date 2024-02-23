a1d0a0b4dd170753e78f7ec574c23447708d31cd
Erick H <erickhv164@gmail.com>
2024-02-23 12:57:01 -0500

    recuperacion

---
+++ src/main/java/CONTROLADOR/pruebaBDD.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CONTROLADOR;
+
+/**
+ *
+ * @author 59398
+ */
+public class pruebaBDD {
+    public static void main(String[] args) {
+        ConexionBDD conectar=new ConexionBDD();
+        conectar.conectar();
+    }
+    
+}
---
+++ src/main/java/MODELO/Libro.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODELO;
+
+public class Libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private int paginas;
+    private int edicion;
+    private String editorial;
+    private String lugar;
+    private String fechaEdicion;
+
+    public Libro(int id, String titulo, String autor, String ISBN, int paginas, int edicion, String editorial, String lugar, String fechaEdicion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.paginas = paginas;
+        this.edicion = edicion;
+        this.editorial = editorial;
+        this.lugar = lugar;
+        this.fechaEdicion = fechaEdicion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(int edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getFechaEdicion() {
+        return fechaEdicion;
+    }
+
+    public void setFechaEdicion(String fechaEdicion) {
+        this.fechaEdicion = fechaEdicion;
+    }
+}
+
